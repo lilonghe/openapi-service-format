71c7db54a8702f9d0783d62f69a8c235b9697c0f
longhe.li <lilonghe@outlook.com>
2025-02-13 11:57:46 +0800

    feat: request interceptor demo

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,5 +4,25 @@ const api = new Api({
     baseURL: "",
 });
 
+// api.instance.interceptors.response.use(
+//     (response) => {
+//         if (response.data?.error) {
+//             // Show error
+//         }
+//         return response
+//     },
+//     (error) => {
+//         if (error.status === 401) {
+//             if (window.location.pathname !== '/login') {
+//                 window.location.href = '/login'
+//             }
+//         } else {
+//            // Show error
+//         }
+//         // keep normal flow
+//         return error.response
+//     }
+// )
+
 
 export default api;
